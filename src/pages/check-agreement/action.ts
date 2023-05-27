e12aa4774b21d5ac4a49f776dda0b91a4ecfcd24
rycont <rycont>
2023-05-27 06:08:14 +0000

    feat: show error message when submission failed

--- src/pages/check-agreement/action.ts
+++ src/pages/check-agreement/action.ts
@@ -3,9 +3,14 @@ import { cfetch, toast } from '@/functions'
 
 export default {
     async createOrder(orderInfo: CreateOrderType) {
-        const result: {
-            success: boolean
-        } = await (
+        const result:
+            | {
+                  success: boolean
+              }
+            | {
+                  code: string
+                  message: string
+              } = await (
             await cfetch('order', {
                 method: 'POST',
                 headers: {
@@ -14,6 +19,11 @@ export default {
                 body: JSON.stringify(orderInfo),
             })
         ).json()
+
+        if ('code' in result) {
+            toast(result.message)
+            throw new Error()
+        }
 
         if (!result.success) {
             toast('오류가 발생했습니다. 다시 시도해주세요.')
